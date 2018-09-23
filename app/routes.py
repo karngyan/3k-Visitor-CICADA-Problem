3902d52049910013c2d61a22f2782822353a9f82
karngyan <karngyan@gmail.com>
2018-09-23 06:22:53 +0530

    done

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from app import app
-count = 1
+count = 0
 
 
 @app.route('/')
 def index():
     global count
     count+=1
-    return f"Hello, World!{count}"
+    if(count%1000 == 0):
+        return 'Hard work or Lazy-Smart Work? Both pays but the amount is different ;) flag{ Good_Work_Coder }' 
+    return f"You are visitor number: {count}. \nVisit this website 1000 times to get the reward."
